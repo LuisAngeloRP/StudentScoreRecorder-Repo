f65d03a79002715ec7eac5eecf753b5a55aa2fd3
LuisAngeloRP <luisangelorp26@gmail.com>
2024-05-29 12:47:10 -0500

    (ADD ST)

--- modeloprueba.py
+++ modeloprueba.py
@@ -1,3 +1,4 @@
+import time
 import streamlit as st
 import pandas as pd
 import os
@@ -6,11 +7,15 @@ from streamlit_gsheets import GSheetsConnection
 
 # Función para obtener la lista de cursos
 def obtener_lista_cursos():
-    with open("cursos.txt", "r") as file:
-        nombres_cursos = file.readlines()
-    nombres_cursos = [nombre.strip() for nombre in nombres_cursos]
-    cursos = [f"curso{i+1}" for i in range(len(nombres_cursos))]
-    return cursos, nombres_cursos
+    try:
+        with open("cursos.txt", "r", encoding="utf-8") as file:
+            nombres_cursos = file.readlines()
+        nombres_cursos = [nombre.strip() for nombre in nombres_cursos]
+        cursos = [f"curso{i+1}" for i in range(len(nombres_cursos))]
+        return cursos, nombres_cursos
+    except UnicodeDecodeError as e:
+        print(f"Error reading the file: {e}")
+        raise
 
 # Función para leer los alumnos de un curso
 def leer_alumnos_curso(curso):
@@ -43,7 +48,6 @@ def crear_tabla(df_alumnos):
         if nuevo_puntaje != puntaje:
             df_alumnos.at[estudiante.Index, 'Puntaje'] = nuevo_puntaje
 
-# Función para descargar archivos binarios
 # Función para descargar archivos binarios
 def get_binary_file_downloader_html(bin_file, file_label='File'):
     # Crear el archivo si no existe
@@ -89,21 +93,31 @@ def tabla_resumen_curso_individual(nombre_curso):
     for sesion in sesiones_guardadas:
         df_sesion_guardada = conn.read(worksheet=sesion, ttl=0)
         tabla_resumen[sesion] = df_sesion_guardada['Puntaje']
+    tabla_resumen.index += 1
     st.dataframe(tabla_resumen)
 
-# Función para mostrar las sesiones guardadas
 def sesiones_guardadas_tab():
     st.subheader("Selecciona una Sesión Guardada")
-    indices_cursos, nombres_cursos = obtener_lista_cursos()
+    indices_cursos, nombres_cursos = obtener_lista_cursos()    
     if not indices_cursos:
         st.warning("No hay cursos creados.")
-        return
-    curso_seleccionado = st.sidebar.selectbox("Selecciona un Curso", nombres_cursos)
+        return   
+    # Retrieve the last selected course from session state
+    last_selected_course = st.session_state.get('last_selected_course', None)   
+    # Set the index of the last selected course if it exists
+    selected_index = nombres_cursos.index(last_selected_course) if last_selected_course in nombres_cursos else 0  
+    curso_seleccionado = st.sidebar.selectbox("Selecciona un Curso", nombres_cursos, index=selected_index)
     indice_curso_seleccionado = indices_cursos[nombres_cursos.index(curso_seleccionado)]
     conn = st.connection(indice_curso_seleccionado, type=GSheetsConnection)
     df_sesiones = conn.read(worksheet="sesiones", ttl=0)
     if df_sesiones is None or df_sesiones.empty:
-        st.info("No hay sesiones guardadas en este curso.")
+        st.info("No hay sesiones guardadas en este curso.")       
+        # Leer el contenido del archivo CSV de cursos si no hay sesiones guardadas
+        ruta_cursos_csv = f"cursos/{curso_seleccionado}/sesiones.csv"
+        if os.path.exists(ruta_cursos_csv):
+            df_cursos = pd.read_csv(ruta_cursos_csv)
+            # Hacer algo con df_cursos, por ejemplo mostrarlo en la interfaz de usuario
+            st.dataframe(df_cursos)       
         return
     df_sesiones = df_sesiones.dropna(subset=['sesiones']).reset_index(drop=True)
     sesiones_guardadas = df_sesiones['sesiones'].tolist()
@@ -122,7 +136,6 @@ def sesiones_guardadas_tab():
                 conn.update(worksheet=sesion_seleccionada, data=df_sesion_guardada)
                 st.success("Cambios guardados exitosamente!")
 
-# Función para agregar una nueva sesión
 def nueva_sesion_ui():
     st.sidebar.subheader("Agregar Nueva Sesión")
     indices_cursos, nombres_cursos = obtener_lista_cursos()
@@ -130,7 +143,9 @@ def nueva_sesion_ui():
         st.sidebar.warning("No hay cursos creados. Cree un curso antes de agregar sesiones.")
         return
     curso_seleccionado = st.sidebar.selectbox("Selecciona el curso", nombres_cursos)
-    indice_curso_seleccionado = indices_cursos[nombres_cursos.index(curso_seleccionado)]
+    indice_curso_seleccionado = indices_cursos[nombres_cursos.index(curso_seleccionado)]   
+    # Store the selected course in session state
+    st.session_state['last_selected_course'] = curso_seleccionado
     nombre_sesion = st.sidebar.text_input("Nombre de la Sesión", "Sesión 1")
     puntaje_maximo = st.sidebar.number_input("Puntaje Máximo por Sesión", value=20)
     fecha_sesion = st.sidebar.date_input("Fecha de la Sesión", pd.Timestamp.now())
@@ -142,16 +157,35 @@ def nueva_sesion_ui():
     conn = st.connection(indice_curso_seleccionado, type=GSheetsConnection)
     if st.sidebar.button("Guardar"):
         nombre_archivo_y_hoja = f"{nombre_sesion}_{puntaje_maximo}_{fecha_sesion.strftime('%Y-%m-%d')}.csv"
-        if validar_puntaje_maximo(edited_df, puntaje_maximo):
-            conn.create(worksheet=nombre_archivo_y_hoja[:-4], data=edited_df)
-            df_sesiones = conn.read(worksheet="sesiones", ttl=0)
-            if df_sesiones is None or df_sesiones.empty:
-                df_sesiones = pd.DataFrame(columns=["sesiones"])
-            sesiones_df = pd.DataFrame({"sesiones": [nombre_archivo_y_hoja[:-4]]})
-            df_sesiones = pd.concat([df_sesiones, sesiones_df]).drop_duplicates().reset_index(drop=True)
-            conn.update(worksheet="sesiones", data=df_sesiones)
-            st.success(f"{nombre_archivo_y_hoja} guardado exitosamente!")
-            st.markdown(get_binary_file_downloader_html(nombre_archivo_y_hoja, 'Descargar archivo'), unsafe_allow_html=True)
+        ruta_directorio = os.path.join("cursos", curso_seleccionado)
+        ruta_archivo = os.path.join(ruta_directorio, nombre_archivo_y_hoja)
+        if not os.path.exists(ruta_directorio):
+            os.makedirs(ruta_directorio)
+        if not os.path.exists(ruta_archivo):
+            if validar_puntaje_maximo(edited_df, puntaje_maximo):
+                with st.spinner(f"Guardando {nombre_archivo_y_hoja}..."):
+                    edited_df.to_csv(ruta_archivo, index=False)
+                st.success(f"{nombre_archivo_y_hoja} guardado exitosamente!")
+                ruta_sesiones = os.path.join("cursos", curso_seleccionado, "sesiones.csv")
+                if os.path.exists(ruta_sesiones):
+                    sesiones_df = pd.read_csv(ruta_sesiones)
+                else:
+                    sesiones_df = pd.DataFrame(columns=["sesiones"])
+                nombre_sesion_sin_extension = nombre_archivo_y_hoja[:-4]
+                sesiones_df = sesiones_df.append({"sesiones": nombre_sesion_sin_extension}, ignore_index=True)
+                sesiones_df.to_csv(ruta_sesiones, index=False)
+                datos_sesion = pd.read_csv(ruta_archivo)
+                conn.create(worksheet=nombre_sesion_sin_extension, data=datos_sesion)
+                df_sesiones = conn.read(worksheet="sesiones", ttl=0)
+                if df_sesiones is None or df_sesiones.empty:
+                    df_sesiones_modificado = sesiones_df
+                else:
+                    df_sesiones_modificado = pd.concat([df_sesiones, sesiones_df]).drop_duplicates().reset_index(drop=True)
+                conn.update(worksheet="sesiones", data=df_sesiones_modificado)
+                time.sleep(2)
+                df_sesiones_actualizado = conn.read(worksheet="sesiones", ttl=0)
+                st.markdown(get_binary_file_downloader_html(ruta_archivo, 'Descargar archivo'), unsafe_allow_html=True)
+
 
 # Función para crear una evaluación
 def crear_evaluacion_ui():
